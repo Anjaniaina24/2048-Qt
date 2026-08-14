4f239fe0161393b51af47e96abe892dbbdc1842c
Anjaniaina24 <nirinjanaharyanjaniaina@gmail.com>
2026-08-14 14:25:19 +0300

    Amelioration du style du Menu

--- Menu.cpp
+++ Menu.cpp
@@ -7,26 +7,48 @@ Menu::Menu(QWidget *parent) : QMainWindow(parent)
     stackedWidget = new QStackedWidget(this);
     setCentralWidget(stackedWidget);
 
+    // --- Widget du menu ---
     menuWidget = new QWidget(this);
+    menuWidget->setObjectName("menuWidget"); // pour le style QSS
+
     QVBoxLayout *menuLayout = new QVBoxLayout(menuWidget);
     menuLayout->setAlignment(Qt::AlignCenter);
-    menuLayout->setSpacing(20);
+    menuLayout->setSpacing(30);
 
+    // Titre "2048"
+    titleLabel = new QLabel("2048", menuWidget);
+    titleLabel->setObjectName("titleLabel");
+    titleLabel->setAlignment(Qt::AlignCenter);
 
+    // Bouton Jouer
     QPushButton *playButton = new QPushButton("Jouer", menuWidget);
-    playButton->setFixedSize(200, 50);
+    playButton->setObjectName("playButton");
+    playButton->setFixedSize(220, 60);
 
-    QPushButton *quitButton = new QPushButton("Quitter",  menuWidget);
-    quitButton->setFixedSize(200, 50);
+    // Bouton Quitter
+    QPushButton *quitButton = new QPushButton("Quitter", menuWidget);
+    quitButton->setObjectName("quitButton");
+    quitButton->setFixedSize(220, 60);
 
+    // Ajout d'une ombre portée sur les boutons (effet moderne)
+    QGraphicsDropShadowEffect *shadow = new QGraphicsDropShadowEffect(this);
+    shadow->setBlurRadius(15);
+    shadow->setOffset(0, 4);
+    shadow->setColor(QColor(0, 0, 0, 80));
+    playButton->setGraphicsEffect(shadow);
+    quitButton->setGraphicsEffect(shadow);
+
+    menuLayout->addWidget(titleLabel);
     menuLayout->addWidget(playButton);
     menuLayout->addWidget(quitButton);
 
+    // --- Widget du jeu ---
     gameWidget = new Game2048(this);
 
     stackedWidget->addWidget(menuWidget);
     stackedWidget->addWidget(gameWidget);
 
+    // Connexions
     connect(playButton, &QPushButton::clicked, this, &Menu::onPlayClicked);
     connect(quitButton, &QPushButton::clicked, this, &Menu::onQuitClicked);
 }
@@ -47,7 +69,7 @@ void Menu::animateToGame()
 
     menuWidget->move(0, 0);
     gameWidget->move(0, height);
-    
+
     QPropertyAnimation *animMenu = new QPropertyAnimation(menuWidget, "pos");
     animMenu->setDuration(500);
     animMenu->setStartValue(menuWidget->pos());
@@ -62,12 +84,10 @@ void Menu::animateToGame()
     group->addAnimation(animMenu);
     group->addAnimation(animGame);
 
-    connect(group, &QParallelAnimationGroup::finished, this, [this]() 
-        {
-            stackedWidget->setCurrentWidget(gameWidget);
-            menuWidget->move(0, 0);
-            gameWidget->move(0, 0);
-        }
-    );
+    connect(group, &QParallelAnimationGroup::finished, this, [this]() {
+        stackedWidget->setCurrentWidget(gameWidget);
+        menuWidget->move(0, 0);
+        gameWidget->move(0, 0);
+    });
     group->start(QAbstractAnimation::DeleteWhenStopped);
 }
--- Menu.hpp
+++ Menu.hpp
@@ -3,29 +3,30 @@
 
 #include "Game2048.hpp"
 #include <QPushButton>
+#include <QLabel>
 #include <QPropertyAnimation>
 #include <QApplication>
 #include <QStackedWidget>
 #include <QMainWindow>
 #include <QParallelAnimationGroup>
+#include <QGraphicsDropShadowEffect>
 
 class Menu : public QMainWindow
 {
     Q_OBJECT
-    public:
-        explicit Menu(QWidget *parent = nullptr);
-    
-    private:
-        QStackedWidget *stackedWidget;
-        QWidget *menuWidget;
-        Game2048 *gameWidget;
-        void animateToGame();
-        void setupMenu();
+public:
+    explicit Menu(QWidget *parent = nullptr);
 
-    private slots:
-        void onPlayClicked();
-        void onQuitClicked();
+private:
+    QStackedWidget *stackedWidget;
+    QWidget *menuWidget;
+    Game2048 *gameWidget;
+    QLabel *titleLabel;      
+    void animateToGame();
 
+private slots:
+    void onPlayClicked();
+    void onQuitClicked();
 };
 
 #endif
